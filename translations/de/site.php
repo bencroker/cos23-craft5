199e68a4c5f23669297c7d1a69c1e46ae2226e10
Ben Croker <ben@putyourlightson.com>
2024-04-10 08:20:37 -0600

    Add translations

--- translations/de/site.php
+++ translations/de/site.php
@@ -3,4 +3,8 @@
 return [
     'Main Text' => 'Haupt Text',
     'The main text for this entry.' => 'Der Haupttext für diesen Eintrag.',
+    'Level of difficulty' => 'Schwierigkeitsgrad',
+    'Preparation time' => 'Vorbereitungszeit',
+    'minutes' => 'Minuten',
+    'easy' => 'einfach',
 ];
